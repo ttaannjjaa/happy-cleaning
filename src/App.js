4dc8b27a931f1b7d4ac7658dd27059d863759987
Tanja Süß <ajnat2268@gmail.com>
2022-02-08 13:02:42 +0100

    use more react commands the first time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import Room from './Room.js';
+import Header from './Header.js';
 
 export default function App() {
+  const rooms = [
+    {
+      text: 'Küche',
+      description: 'Herdplatten nicht vergessen',
+      isDescriptionVisible: true,
+      isClean: true,
+    },
+    {
+      text: 'Wohnzimmer',
+      description: 'Staubwischen auch auf dem Fensterbrett',
+      isDescriptionVisible: true,
+      isClean: false,
+    },
+    {
+      text: 'Bad',
+      description: 'eigener Lappen für das WC',
+    },
+  ];
+
   return (
     <main className="App">
-      <Room text="Küche" isClean />
-      <Room text="Wohnzimmer" isClean={false} />
-      <Room text="Bad" />
+      <Header>Happy Cleaning</Header>
+      {rooms.map(({ text, description, isDescriptionVisible, isClean }) => (
+        <Room
+          key={text}
+          text={text}
+          description={description}
+          isDescriptionVisible={isDescriptionVisible}
+          isClean={isClean}
+        />
+      ))}
     </main>
   );
 }
--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,21 @@
 import './Room.css';
 
-export default function Room({ text, isClean }) {
+export default function Room({
+  text,
+  isClean,
+  description,
+  isDescriptionVisible,
+}) {
+  const statusClassName = isClean
+    ? 'Room__status Room__status--clean'
+    : 'Room__status Room__status--dirty';
   return (
     <section className="Room">
-      {text} {isClean ? '🍏' : '😷'}
+      {text}
+      {isDescriptionVisible && <p className="TextDescription">{description}</p>}
+      <div className={statusClassName}></div>
     </section>
   );
 }
+
+//<p hidden={!isDescriptionVisible}>{description}}</p>//
