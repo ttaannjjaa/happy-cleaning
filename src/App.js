7f96d5e51918cd02c826bf3bccadce88ea5ee900
Tanja Süß <ajnat2268@gmail.com>
2022-02-09 23:19:51 +0100

    make update first file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Room from './Room.js';
 import Header from './Header.js';
+import { useImmer } from 'use-immer';
 
 export default function App() {
-  const rooms = [
+  const [rooms, updateRooms] = useImmer([
+    //state Verwendung mit IMMER
     {
       text: 'Küche',
       description: 'Herdplatten nicht vergessen',
@@ -20,20 +22,33 @@ export default function App() {
       text: 'Bad',
       description: 'eigener Lappen für das WC',
     },
-  ];
-
+  ]);
   return (
     <main className="App">
       <Header>Happy Cleaning</Header>
-      {rooms.map(({ text, description, isDescriptionVisible, isClean }) => (
-        <Room
-          key={text}
-          text={text}
-          description={description}
-          isDescriptionVisible={isDescriptionVisible}
-          isClean={isClean}
-        />
-      ))}
+      {rooms.map(
+        ({ text, description, isDescriptionVisible, isClean }, index) => (
+          <Room
+            key={text}
+            text={text}
+            description={description}
+            isDescriptionVisible={isDescriptionVisible}
+            isClean={isClean}
+            toggleStatus={event => {
+              event.stopPropagation();
+              updateRooms(draft => {
+                draft[index].isClean = !isClean;
+              }); //funktion by IMMER
+              //ersetzt ohne IMMER mit setState
+              // setRooms([
+              //   ...rooms.slice(0, index),
+              //   { ...rooms[index], isClean: !isClean },
+              //   ...rooms.slice(index + 1),
+              // ]);
+            }}
+          />
+        )
+      )}
     </main>
   );
 }
