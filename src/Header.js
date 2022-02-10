44c7ff8c3687b798ae265cb1a807821952890335
Tanja Süß <ajnat2268@gmail.com>
2022-02-10 22:51:50 +0100

    add color changing for the 2 buttons

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
-import './Header.css';
+import styled from 'styled-components';
 
 export default function Header({ children }) {
-  return <h1 className="Header">{children}</h1>;
+  return <HeaderCont className="Header">{children}</HeaderCont>;
 }
+
+const HeaderCont = styled.h1`
+  border-bottom: 4px solid yellowgreen;
+  border-top: 4px solid yellowgreen;
+  border-radius: 5px;
+  text-align: center;
+  padding: 5px;
+`;
